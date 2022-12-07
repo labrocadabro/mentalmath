fe8026967fe0df0ea934f8a079d0a2fccc4d8bb5
Laura <moomal.abro@gmail.com>
2022-12-07 10:42:27 +0500

    install sveltekit and tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+};
+
+export default config;
